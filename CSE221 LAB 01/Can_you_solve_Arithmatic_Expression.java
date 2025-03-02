3138474d1ae0d36e7306e69c21a9ea9d3cb16dea
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 05:49:38 +0600

    Update Can_you_solve_Arithmatic_Expression.java

--- CSE221 LAB 01/Can_you_solve_Arithmatic_Expression.java
+++ CSE221 LAB 01/Can_you_solve_Arithmatic_Expression.java
@@ -1,32 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Can_you_solve_Arithmatic_Expression {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int input = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i< input; i++){
-            String string = sc.nextLine();
-            String[] mod_string = string.split(" ");
-            float operand_1 = Integer.parseInt(mod_string[1]);
-            float operand_2 = Integer.parseInt(mod_string[3]);
-            String operator =mod_string[2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int input = Integer.parseInt(br.readLine());
+        for (int i = 0; i < input; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken(); // Ignore the first token
+            float operand_1 = Integer.parseInt(st.nextToken());
+            String operator = st.nextToken();
+            float operand_2 = Integer.parseInt(st.nextToken());
+            
             switch (operator) {
                 case "+":
-                    System.out.println(operand_1+operand_2);
+                    System.out.println(operand_1 + operand_2);
                     break;
                 case "-":
-                    System.out.println(operand_1-operand_2);
+                    System.out.println(operand_1 - operand_2);
                     break;
                 case "*":
-                    System.out.println(operand_1*operand_2);          
+                    System.out.println(operand_1 * operand_2);
                     break;
-                case "/":  
-                    System.out.println(operand_1/operand_2);          
-                    break;                                                                                                                                                                               
+                case "/":
+                    System.out.println(operand_1 / operand_2);
+                    break;
                 default:
                     break;
             }
         }
-        sc.close();
     }
 }
