ea4e0df7494bc3fac3bf8dfea20d10d9b2512cf3
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 05:37:21 +0600

    Update Two_Sum_Trouble.java

--- CSE221 LAB 02/Two_Sum_Trouble.java
+++ CSE221 LAB 02/Two_Sum_Trouble.java
@@ -1,31 +1,36 @@
-import java.util.Scanner;
-
-public class Two_Sum_Trouble{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String[] n = sc.nextLine().split(" ");
-        int len = Integer.parseInt(n[0]), target = Integer.parseInt(n[1]), idx_1 = 0, idx_2 = len-1;
-        String[] temp = sc.nextLine().split(" "); 
-        sc.close();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Two_Sum_Trouble {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int len = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        
         int[] arr = new int[len];
-        for (int i = 0; i < len; i++){
-            arr[i] = Integer.parseInt(temp[i]);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < len; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        int idx_1 = 0, idx_2 = len - 1;
         boolean flag = false;
-        while (idx_1<idx_2){
-            if (arr[idx_1]+arr[idx_2] == target){
-                System.out.println((idx_1+1) +" "+ (idx_2+1));
+        while (idx_1 < idx_2) {
+            if (arr[idx_1] + arr[idx_2] == target) {
+                System.out.println((idx_1 + 1) + " " + (idx_2 + 1));
                 flag = true;
                 break;
-            }
-            else if (arr[idx_1]+arr[idx_2] < target){
+            } else if (arr[idx_1] + arr[idx_2] < target) {
                 idx_1 += 1;
-            }
-            else {
+            } else {
                 idx_2 -= 1;
             }
         }
-        if (flag == false){
+        
+        if (!flag) {
             System.out.println(-1);
         }
     }
