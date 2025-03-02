e466882203924977b4da88e5418855cd6523fed1
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 05:35:39 +0600

    Update Longest_Subarray_Sum.java

--- CSE221 LAB 02/Longest_Subarray_Sum.java
+++ CSE221 LAB 02/Longest_Subarray_Sum.java
@@ -1,24 +1,31 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
 public class Longest_Subarray_Sum {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] temp = sc.nextLine().split(" ");
-        int n = Integer.parseInt(temp[0]), k = Integer.parseInt(temp[1]), sum = 0, l = 0, count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
         int[] arr = new int[n];
-        String[] input2 = sc.nextLine().split(" ");
-        sc.close();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(input2[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i< n; i++){
+        
+        int sum = 0, l = 0, count = 0;
+        for (int i = 0; i < n; i++) {
             sum += arr[i];
-            while (sum > k){
+            while (sum > k) {
                 sum -= arr[l];
                 l += 1;
             }
             count = Math.max(count, i - l + 1);
         }
+        
         System.out.println(count);
     }
 }
