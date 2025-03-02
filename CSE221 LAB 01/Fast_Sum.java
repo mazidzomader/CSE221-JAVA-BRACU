6613905cfecff2bb1d55a976d74f39e5d57ea9d4
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 05:51:36 +0600

    Update Fast_Sum.java

--- CSE221 LAB 01/Fast_Sum.java
+++ CSE221 LAB 01/Fast_Sum.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Fast_Sum {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int input = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < input; i ++){
-            int num = Integer.parseInt(sc.nextLine());
-            System.out.println((num*(num+1))/2);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int input = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < input; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            System.out.println((num * (num + 1)) / 2);
         }
-        sc.close();
     }
 }
