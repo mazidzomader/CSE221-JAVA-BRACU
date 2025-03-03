3495aaaf8d0687536f1b84604078b0c78ea2699d
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 06:00:47 +0600

    Update Trains.java

--- CSE221 LAB 01/Trains.java
+++ CSE221 LAB 01/Trains.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.StringTokenizer;
+ 
 public class Trains {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(st.nextToken());
         List<String[]> data = new ArrayList<>();
-
+ 
         for (int i = 0; i < n; i++) {
-            String trainDetails = scanner.nextLine();
+            String trainDetails = reader.readLine();
             data.add(modification(trainDetails));
         }
-
+ 
         List<String[]> afterRearrangement = unknownStableSort(data, n);
-
+ 
         for (String[] train : afterRearrangement) {
             System.out.println(train[3]);
         }
-        scanner.close();
     }
-
+ 
     public static String[] modification(String trainDetails) {
         String[] temp = trainDetails.split(" ");
         String name = temp[0];
@@ -28,13 +31,13 @@ public class Trains {
         String time = temp[6];
         return new String[]{name, destination, time, trainDetails};
     }
-
+ 
     public static int timeToMinuteConverter(String time) {
         int hour = Integer.parseInt(time.substring(0, 2));
         int minute = Integer.parseInt(time.substring(3));
         return hour * 60 + minute;
     }
-
+ 
     public static List<String[]> unknownStableSort(List<String[]> data, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -50,4 +53,3 @@ public class Trains {
         return data;
     }
 }
-
