d8b5c6fe552841cf26aefdc025a971316191934d
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 06:03:19 +0600

    Update Odd_or_Even.java

--- CSE221 LAB 01/Odd_or_Even.java
+++ CSE221 LAB 01/Odd_or_Even.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Odd_or_Even {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for (int i = 0; i < num; i++){
-            int number = sc.nextInt();
-            String result = (number % 2 == 0) ? number+" is an Even number.": number + " is an Odd number.";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(reader.readLine());
+        StringTokenizer st;
+        
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(reader.readLine());
+            int number = Integer.parseInt(st.nextToken());
+            String result = (number % 2 == 0) ? number + " is an Even number." : number + " is an Odd number.";
             System.out.println(result);
         }
-        sc.close();
     }
 }
