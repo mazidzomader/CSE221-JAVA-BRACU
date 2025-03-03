bcaa765c2357264c6bc5d7521b61e58bfd2a625d
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-03 06:02:21 +0600

    Update Bubble_Sort.java

--- CSE221 LAB 01/Bubble_Sort.java
+++ CSE221 LAB 01/Bubble_Sort.java
@@ -1,28 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Bubble_Sort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int first = Integer.parseInt(sc.nextLine());
-        String temp = sc.nextLine();
-        sc.close();
-        String[] second = temp.split(" ");
-        for (int i = 0; i < first-1; i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int first = Integer.parseInt(reader.readLine());
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        
+        String[] second = new String[first];
+        int index = 0;
+        
+        while (st.hasMoreTokens()) {
+            second[index++] = st.nextToken();
+        }
+
+        for (int i = 0; i < first - 1; i++) {
             boolean flag = false;
-            for (int j = 0; j < first-1-i; j++ ){
-                if (Integer.parseInt(second[j]) > Integer.parseInt(second[j+1])){
+            for (int j = 0; j < first - 1 - i; j++) {
+                if (Integer.parseInt(second[j]) > Integer.parseInt(second[j + 1])) {
                     flag = true;
-                    temp = second[j]; 
-                    second[j] = second[j+1];
-                    second[j+1] = temp;
+                    String temp = second[j];
+                    second[j] = second[j + 1];
+                    second[j + 1] = temp;
                 }
             }
-            if (!flag){
+            if (!flag) {
                 break;
             }
         }
-        for (int i = 0; i < first; i++ ){
-            System.out.print(second[i]+" ");
+
+        for (int i = 0; i < first; i++) {
+            System.out.print(second[i] + " ");
         }
     }
 }
